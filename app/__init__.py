d7e606809ce3d249880de082693a9bb967a64150
PablitoAM <poloharvin@hotmail.com>
2016-02-06 14:08:58 +0100

    Little fixes

--- app/__init__.py
+++ app/__init__.py
@@ -31,12 +31,28 @@ couchdb = getCouchDB(None, app)
 boostrapServers = app.config['BOOTSTRAP_SERVERS']
 topic = app.config['MESSAGE_TOPIC']
 
-producer = None
 consumer = None
+producer = None
 
-from app.services.message_services import initConsumer, initProducer
-initProducer(producer, topic, boostrapServers)
-initConsumer(consumer, topic, boostrapServers)
+# Initialises the consumer
+def initConsumer(topic, bootstrapServers):
+  if(consumer == None):
+    try:
+      consumer = KafkaConsumer(topic, bootstrap_servers=boostrapServers)
+      logger.info("Consumer Created")
+    except Exception, e:
+      logger.error(str(e))
+  return consumer
+
+# Initialises the producer
+def initProducer(topic, boostrapServers):
+  if(producer == None):
+    try:
+      producer = KafkaProducer(bootstrap_servers=bootstrapServers)
+      logger.info("Producer Created")
+    except Exception, e:
+      logger.error(str(e))
+  return producer
 
 # ===============
 # Imports
--- app/services/message_services.py
+++ app/services/message_services.py
@@ -1,4 +1,3 @@
-from app import producer, consumer
 from kafka import KafkaConsumer
 from kafka import KafkaProducer
 
@@ -9,25 +8,8 @@ logger = logging.getLogger(__name__)
 # Kakfa
 # ===============
 
-def initConsumer(consumer, topic, bootstrapServers):
-  if(consumer == None):
-    try:
-      consumer = KafkaConsumer(topic, bootstrap_servers=boostrapServers)
-      logger.info("Consumer Created")
-    except Exception, e:
-      logger.error(str(e))
-  return consumer
-
-def initProducer(producer, topic, boostrapServers):
-  if(producer == None):
-    try:
-      producer = KafkaProducer(bootstrap_servers=bootstrapServers)
-      logger.info("Producer Created")
-    except Exception, e:
-      logger.error(str(e))
-  return producer
-
-def consumeMessages():
+def consumeMessages(consumer):
+    initConsumer()
     for message in consumer:
         # message value and key are raw bytes -- decode if necessary!
         # e.g., for unicode: `message.value.decode('utf-8')`
@@ -35,5 +17,6 @@ def consumeMessages():
                                              message.offset, message.key,
                                              message.value))
         
-def produceMessage(topic, key, message):
+def produceMessage(producer, topic, key, message):
+    initProducer()
     producer.send(topic, { key : message})
